320b8fc4f28445d59aa5cd26e00fd350a147fb64
MrOats <snarkatt@gmail.com>
2016-10-09 22:08:41 -0500

    Added ability to enable and disable update types

--- batch/Update_WindowsDefender.bat
+++ batch/Update_WindowsDefender.bat
@@ -2,8 +2,13 @@
 :: Last updated 2016-10-09
 :: Updates Windows Defender's Anti-Malware and Anti-Spyware Signatures
 :: Best if added to Task Scheduler...
-
+set bool_EnableVirusCheck=true
+set bool_EnableSpywareCheck=true
 :: Update Virus/Malware Definitions first...
-"C:\Program Files\Windows Defender\MpCmdRun.exe" -SignatureUpdate
+if %bool_EnableVirusCheck% EQU true (
+  "C:\Program Files\Windows Defender\MpCmdRun.exe" -SignatureUpdate
+)
 :: Update Spyware Definitons next...
-"C:\Program Files\Windows Defender\MpCmdRun.exe" -SignatureUpdate -MMPC
+if %bool_EnableSpywareCheck% EQU true (
+  "C:\Program Files\Windows Defender\MpCmdRun.exe" -SignatureUpdate -MMPC
+)
